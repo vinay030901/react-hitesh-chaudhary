75806ff12eee667c3b0656abf60e6838f18f635c
Vinay Kumar <champ.vk91@gmail.com>
2023-11-21 00:04:35 +0530

    app

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  let counter = 5;
+  // let counter = 5;
   const addValue = () => {
-    console.log("value added ", counter++);
+    console.log("value added ", counter);
+    if (counter < 20) setCounter(counter + 1);
   };
+  const removeValue = () => {
+    if (counter >= 1) setCounter(counter - 1);
+  };
+
+  let [counter, setCounter] = useState(15);
+
   return (
     <>
       <h1> chai aur reacts</h1>
@@ -12,7 +20,7 @@ function App() {
 
       <button onClick={addValue}>add value</button>
       <br />
-      <button>remove value</button>
+      <button onClick={removeValue}>remove value</button>
     </>
   );
 }
